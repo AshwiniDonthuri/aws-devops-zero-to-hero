31481d69557e6035f1086b97ed3b8272038aa7c5
AshwiniDonthuri <54536061+AshwiniDonthuri@users.noreply.github.com>
2024-03-26 13:59:57 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ashwinif5/simple-python-flask-service
+docker pull ashwinif5/simple-python-codebuild
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ashwinif5/simple-python-flask-service
+docker run -d -p 5000:5000 ashwinif5/simple-python-codebuild
